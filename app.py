60f90d7f15008a0e7be81530364173cedf858922
vickydasta <vickydasta@gmail.com>
2016-02-13 00:35:10 +0700

    add brain/ app.py

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+from bs4 import BeautifulSoup as bs
+import aiml 
+
+bot = aiml.Kernel()
+try:
+    print '[+] loading bot brain'
+    bot.learn('brain/brain.aiml')
+    print '[+] brain loaded'
+except Exception as e:
+    raise Exception(e)
+
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    return render_template("index.html")
+
+@app.route('/apps', methods=['GET','POST'])
+def echo():
+    answer = bot.respond(request.form['text'])
+    
+    return render_template("index.html",data=answer)
+
+if __name__ == "__main__":
+	app.run(threaded=True, debug = True, host='0.0.0.0', port=8888, passthrough_errors=True)
